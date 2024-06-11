d9ca0ca7be6ae5ffdf0e17b7d98154d410dd5a53
AndreLNC <boosgamer112@gmail.com>
2024-06-10 21:35:07 -0300

    Add files via upload

---
+++ algoritimos de ordenação/merge_sort.py
@@ -0,0 +1,34 @@
+def merge_sort(lista):
+    if len(lista) > 1:
+        metade = len(lista) // 2
+
+        metade1 = lista[:metade]
+        metade2 = lista[metade:]
+
+        merge_sort(metade1)
+        merge_sort(metade2)
+
+        i = j = k = 0
+
+        while i < len(metade1) and j < len(metade2):
+            if metade1[i] < metade2[j]:
+                lista[k] = metade1[i]
+                i += 1
+            else:
+                lista[k] = metade2[j]
+                j += 1
+            k += 1
+
+        while i < len(metade1):
+            lista[k] = metade1[i]
+            i += 1
+            k += 1
+
+        while j < len(metade2):
+            lista[k] = metade2[j]
+            j += 1
+            k += 1
+
+lista = [12, 11, 13, 5, 6, 7]
+merge_sort(lista)
+print("Lista ordenada é:", lista)
